48d2ca84afddd9967d67447a5602aded9a784f90
LLiyilin <117006952+LLiyilin@users.noreply.github.com>
2022-10-30 23:15:15 +0800

    Add files via upload

---
+++ StudentFileReader.java
@@ -0,0 +1,31 @@
+package cn.edu.swu.homework;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+class StudentFileReader extends StudentAbstractReader {
+
+    private File file;
+
+    public StudentFileReader(File file) {
+
+        this.setFile(file);
+    }
+
+    @Override
+    public InputStream getInputStream() throws FileNotFoundException {
+        return new FileInputStream(this.getFile());
+    }
+
+    public File getFile() {
+
+        return file;
+    }
+
+    public void setFile(File file) {
+
+        this.file = file;
+    }
+}
